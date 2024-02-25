93a76f218656dad135e46255d2095bb6522f7200
Pierre Chavez <pierre_chavez@outlook.com>
2024-02-24 23:04:45 -0500

    names by types and functions

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,50 @@
+(() => {
+
+    // función para obtener información de una película por Id
+    function getMovieInformationById(id: string) {
+        console.log({ id });
+    }
+
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getMovieActorOrCastInformation(movieId: string) {
+        console.log({ movieId });
+    }
+
+    // funcion para obtener el bio del actor por el id
+    function getActorOrCastBioById(id: string) {
+        console.log({ id });
+    }
+
+    interface Movie {
+        title: string;
+        description: string;
+        rating: number;
+        cast: string[]
+    }
+
+    // Crear una película
+    function createMovie({ title, description, rating, cast }: Movie) {
+        console.log({ title, description, rating, cast });
+    }
+
+    // Crea un nuevo actor
+    function createMovieActor(fullName: string, birthDate: Date): boolean {
+
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if (fullName === 'fernando') return false;
+
+        console.log('Crear actor');
+        return true;
+
+    }
+
+
+
+
+})();
+
+
+
+
