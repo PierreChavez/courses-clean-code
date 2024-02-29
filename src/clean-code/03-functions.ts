6a50ef0825e4428112816c71ef0f3e7b1673ebad
Pierre Chavez <pierre_chavez@outlook.com>
2024-02-29 08:03:22 -0500

    functions

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,50 +1,49 @@
 (() => {
+  // función para obtener información de una película por Id
+  function getMovieInformationById(id: string) {
+    console.log({ id });
+  }
 
-    // función para obtener información de una película por Id
-    function getMovieInformationById(id: string) {
-        console.log({ id });
-    }
+  // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+  function getMovieActorOrCastInformation(movieId: string) {
+    console.log({ movieId });
+  }
 
-    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-    function getMovieActorOrCastInformation(movieId: string) {
-        console.log({ movieId });
-    }
+  // funcion para obtener el bio del actor por el id
+  function getActorOrCastBioById(id: string) {
+    console.log({ id });
+  }
 
-    // funcion para obtener el bio del actor por el id
-    function getActorOrCastBioById(id: string) {
-        console.log({ id });
-    }
+  interface Movie {
+    title: string;
+    description: string;
+    rating: number;
+    cast: string[];
+  }
 
-    interface Movie {
-        title: string;
-        description: string;
-        rating: number;
-        cast: string[]
-    }
+  // Crear una película
+  function createMovie({ title, description, rating, cast }: Movie) {
+    console.log({ title, description, rating, cast });
+  }
 
-    // Crear una película
-    function createMovie({ title, description, rating, cast }: Movie) {
-        console.log({ title, description, rating, cast });
-    }
+  // Crea un nuevo actor
+  function createMovieActor(fullName: string, birthDate: Date): boolean {
+    // tarea asincrona para verificar nombre
+    // ..
+    // ..
+    if (fullName === "fernando") return false;
 
-    // Crea un nuevo actor
-    function createMovieActor(fullName: string, birthDate: Date): boolean {
+    console.log("Crear actor");
+    return true;
+  }
 
-        // tarea asincrona para verificar nombre
-        // ..
-        // ..
-        if (fullName === 'fernando') return false;
+  const getPayAmount = ({isDead = false, isSeparated = true, isRetired = false}): number => {
 
-        console.log('Crear actor');
-        return true;
+    if (isDead) return 1500;
 
-    }
+    if (isSeparated) return 2500;
 
+    return isRetired ? 3000 : 4000;
 
-
-
+  };
 })();
-
-
-
-
