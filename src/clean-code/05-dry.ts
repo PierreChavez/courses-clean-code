ef068bf3843f1402b320f4fc78279861f6ceccfa
Pierre Chavez <pierre_chavez@outlook.com>
2024-03-03 23:55:53 -0500

    dry end

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -39,7 +39,7 @@ class Product {
 }
 
 (() => {
-  const macbook = new Product("Macbook Pro", 100);
+  const macbook = new Product("Macbook Pro", 100, "M1");
 
   console.log(macbook.toString());
 })();
